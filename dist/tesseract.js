1331ef8f18a3217795811b13ed5782df0bb34d33
Kevin Kwok <antimatter15@gmail.com>
2016-10-07 23:12:03 -0400

    this is only mildly annoying

--- dist/tesseract.js
+++ dist/tesseract.js
@@ -4,7 +4,7 @@
 exports.defaultOptions = {
     langPath: 'https://cdn.rawgit.com/naptha/tessdata/gh-pages/3.02/',
     // workerPath: 'dist/worker.js',
-    workerPath: 'https://cdn.rawgit.com/naptha/tesseract.js/0.1.0/dist/worker.js',
+    workerPath: 'https://cdn.rawgit.com/naptha/tesseract.js/0.1.3/dist/worker.js',
     tesseractPath: 'https://cdn.rawgit.com/naptha/tesseract.js-core/0.1.0/index.js'
 };
 
