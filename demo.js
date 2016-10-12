244d0b98d78b68bc7485394d10538b31c4488d2a
Kevin Kwok <antimatter15@gmail.com>
2016-10-12 04:27:48 -0400

    fixing demo

--- demo.js
+++ demo.js
@@ -2,8 +2,8 @@ var input = document.getElementById('input')
 var input_overlay = document.getElementById('input-overlay')
 var ioctx = input_overlay.getContext('2d')
 // var output = document.getElementById('output')
-var output_overlay = document.getElementById('output-overlay')
-var output_text = document.getElementById('output-text')
+// var output_overlay = document.getElementById('output-overlay')
+var output_text = document.getElementById('log')
 
 var demo_instructions = document.getElementById('demo-instructions')
 
@@ -28,6 +28,8 @@ var lang_drop_instructions = {
 function setUp(){
 	input_overlay.width = input.naturalWidth
 	input_overlay.height = input.naturalHeight
+
+	output_text.style.height = input.height + 'px'
 }
 
 setUp()
@@ -47,7 +49,7 @@ function startDemo(){
 
 	function start(){
 		Tesseract.recognize(input)
-		.progress(progress)
+		.progress(progressUpdate)
 		.then(result)
 
 		input.removeEventListener('load', start)
@@ -57,25 +59,64 @@ function startDemo(){
 	else input.addEventListener('load', start)
 }
 
-function progress(p){
-	var text = JSON.stringify(p)
+// function progress(p){
+// 	var text = JSON.stringify(p)
 
-	// octx.clearRect(0, 0, output.width, output.height)
+// 	// octx.clearRect(0, 0, output.width, output.height)
 
-	// octx.textAlign = 'center'
-	// octx.fillText(text, output.width/2, output.height/2)
-	output_overlay.style.display = 'block'
-	output_overlay.innerHTML += output_overlay.innerHTML.length ? "\n" + text : text
-	output_overlay.scrollTop = output_overlay.scrollHeight;
+// 	// octx.textAlign = 'center'
+// 	// octx.fillText(text, output.width/2, output.height/2)
+// 	output_overlay.style.display = 'block'
+// 	output_overlay.innerHTML += output_overlay.innerHTML.length ? "\n" + text : text
+// 	output_overlay.scrollTop = output_overlay.scrollHeight;
+// }
+
+
+function progressUpdate(packet){
+	var log = document.getElementById('log');
+
+	if(log.firstChild && log.firstChild.status === packet.status){
+		if('progress' in packet){
+			var progress = log.firstChild.querySelector('progress')
+			progress.value = packet.progress
+		}
+	}else{
+		var line = document.createElement('div');
+		line.status = packet.status;
+		var status = document.createElement('div')
+		status.className = 'status'
+		status.appendChild(document.createTextNode(packet.status))
+		line.appendChild(status)
+
+		if('progress' in packet){
+			var progress = document.createElement('progress')
+			progress.value = packet.progress
+			progress.max = 1
+			line.appendChild(progress)
+		}
+
+
+		if(packet.status == 'done'){
+			var pre = document.createElement('pre')
+			pre.appendChild(document.createTextNode(packet.data.text))
+			line.innerHTML = ''
+			line.appendChild(pre)
+
+		}
+
+		log.insertBefore(line, log.firstChild)
+	}
 }
 
 function result(res){
 	// octx.clearRect(0, 0, output.width, output.height)
 	// octx.textAlign = 'left'
 
-	console.log('result was:',res)
-	output_overlay.style.display = 'none'
-	output_text.innerHTML = res.text
+	console.log('result was:', res)
+	// output_overlay.style.display = 'none'
+	// output_text.innerHTML = res.text
+
+	progressUpdate({ status: 'done', data: res })
 
 	res.words.forEach(function(w){
 		var b = w.bbox;
@@ -129,10 +170,10 @@ function play(){
 	demo_instructions.style.display = 'none'
 	output_text.style.display = 'block'
 	output_text.innerHTML = ''
-	output_overlay.innerHTML = ''
+	// output_overlay.innerHTML = ''
 
 	Tesseract.recognize(input, language)
-	.progress( progress )
+	.progress( progressUpdate )
 	.then( result )
 }
 
