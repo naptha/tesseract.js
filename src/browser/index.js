244a948991138a6988f2cd17ef28cb8ef64e0da2
Jerome Wu <jeromewus@gmail.com>
2018-11-15 10:09:05 +0800

    Use jsDelivr as cdn

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,7 +1,7 @@
 var defaultOptions = {
-    // workerPath: 'https://rawcdn.githack.com/naptha/tesseract.js/0.2.0/dist/worker.js',
-    corePath: 'https://rawcdn.githack.com/naptha/tesseract.js-core/0.1.0/index.js',    
-    langPath: 'https://rawcdn.githack.com/naptha/tessdata/gh-pages/3.02/',
+    // workerPath: 'https://cdn.jsdelivr.net/gh/naptha/tesseract.js@0.2.0/dist/worker.js',
+    corePath: 'https://cdn.jsdelivr.net/gh/naptha/tesseract.js-core@0.1.0/index.js',    
+    langPath: 'https://cdn.jsdelivr.net/gh/naptha/tessdata@gh-pages/3.02/',
 }
 
 if (process.env.NODE_ENV === "development") {
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "development") {
     defaultOptions.workerPath = location.protocol + '//' + location.host + '/dist/worker.dev.js?nocache=' + Math.random().toString(36).slice(3)
 }else{
     var version = require('../../package.json').version;
-    defaultOptions.workerPath = 'https://rawcdn.githack.com/naptha/tesseract.js/' + version + '/dist/worker.js'
+    defaultOptions.workerPath = 'https://cdn.jsdelivr.net/gh/naptha/tesseract.js@' + version + '/dist/worker.js'
 }
 
 exports.defaultOptions = defaultOptions;
