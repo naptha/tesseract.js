e9cde564e5280751dd3e6a03ff7483aba56007ce
Jerome Wu <jeromewus@gmail.com>
2018-11-20 18:30:23 +0800

    Fix lint error

--- src/browser/worker.js
+++ src/browser/worker.js
@@ -1,23 +1,20 @@
-const workerUtils = require('../common/worker.js')
+const workerUtils = require('../common/worker.js');
 
-if (process.env.NODE_ENV === "development") {
-    console.debug('Using Development Worker')
+if (process.env.NODE_ENV === 'development') {
+  console.debug('Using Development Worker');
 }
 
-global.addEventListener('message', function(e){
-    var packet = e.data;
-    workerUtils.dispatchHandlers(packet, obj => postMessage(obj))
-})
+global.addEventListener('message', ({ data }) => {
+  workerUtils.dispatchHandlers(data, obj => postMessage(obj));
+});
 
-exports.getCore = function(req, res){
-    if(!global.TesseractCore){
-        res.progress({ status: 'loading tesseract core', progress: 0 })
-        importScripts(req.workerOptions.corePath)
-        res.progress({ status: 'loading tesseract core', progress: 1 })
+workerUtils.setAdapter({
+  getCore: (req, res) => {
+    if (!global.TesseractCore) {
+      res.progress({ status: 'loading tesseract core', progress: 0 });
+      global.importScripts(req.workerOptions.corePath);
+      res.progress({ status: 'loading tesseract core', progress: 1 });
     }
-    return TesseractCore
-}
-
-exports.getLanguageData = require('./lang.js')
-
-workerUtils.setAdapter(module.exports);
+    return global.TesseractCore;
+  },
+});
