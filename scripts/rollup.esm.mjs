f0a6c0813f56ce1aabeabe7c6d8e2f64d74ef517
Balearica <admin@scribeocr.com>
2023-05-15 17:56:42 -0700

    Added source maps to esm build per #761

--- scripts/rollup.esm.mjs
+++ scripts/rollup.esm.mjs
@@ -1,4 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default [
   {
@@ -6,8 +7,13 @@ export default [
     output: {
       file: 'dist/tesseract.esm.min.js',
       format: 'esm',
+      sourcemap: true,
       banner: '/* eslint-disable */',
     },
-    plugins: [commonjs()],
+    plugins: [
+      sourcemaps(),
+      commonjs()
+
+    ],
   },
 ];
