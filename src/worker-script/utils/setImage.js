3e60bf4748b1f4a9e4f579829ad40572d1d57e8a
Balearica <admin@scribeocr.com>
2023-06-20 19:21:32 -0700

    Fixed orientation metadata detection and added unit tests per #783 (#784)

--- src/worker-script/utils/setImage.js
+++ src/worker-script/utils/setImage.js
@@ -11,7 +11,7 @@ module.exports = (TessModule, api, image, angle = 0) => {
   // Check for bmp magic numbers (42 and 4D in hex)
   const isBmp = (image[0] === 66 && image[1] === 77) || (image[1] === 66 && image[0] === 77);
 
-  const exif = image.slice(0, 500).toString().match(/\x01\x12\x00\x03\x00\x00\x00\x01\x00(.)/)?.[1]?.charCodeAt(0) || 1;
+  const exif = parseInt(image.slice(0, 500).join(' ').match(/1 18 0 3 0 0 0 1 0 (\d)/)?.[1], 10) || 1;
 
   // /*
   //  * Leptonica supports some but not all bmp files
