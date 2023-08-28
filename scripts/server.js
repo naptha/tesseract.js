d0df3d126cc80885c2f6c18e7c6053de2977b657
Balearica <admin@scribeocr.com>
2023-08-27 20:58:29 -0700

    Edited browser benchmark to report memory use per #813 (#817)

--- scripts/server.js
+++ scripts/server.js
@@ -9,8 +9,16 @@ const compiler = webpack(webpackConfig);
 const app = express();
 
 app.use(cors());
-app.use('/', express.static(path.resolve(__dirname, '..')));
 app.use(middleware(compiler, { publicPath: '/dist', writeToDisk: true }));
+
+// These headers are required to measure memory within the benchmark code.
+// If they are problematic within other contexts they can be removed.
+app.use(express.static(path.resolve(__dirname, '..'), {
+  setHeaders: (res) => {
+    res.set('Cross-Origin-Opener-Policy', 'same-origin');
+    res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  }
+}));
 
 module.exports = app.listen(3000, () => {
   console.log('Server is running on the port no. 3000');
