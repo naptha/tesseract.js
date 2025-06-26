4f66de1900ab33a92e48313cfd7c351ccdf2acdb
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-06-27 00:00:31 +0200

    feat(ts): added export for languages in types declaration file

--- src/index.d.ts
+++ src/index.d.ts
@@ -229,6 +229,9 @@ declare namespace Tesseract {
     pdf: number[] | null;
     debug: string | null;
   }
+  // Languages defined in src/constants/languages.js
+  type LanguageKey =  'AFR'  | 'AMH'  | 'ARA'| 'ASM'| 'AZE'| 'AZE_CYRL'| 'BEL'| 'BEN'| 'BOD'| 'BOS'| 'BUL'| 'CAT'| 'CEB'| 'CES'| 'CHI_SIM'| 'CHI_TRA'| 'CHR'| 'CYM'| 'DAN'| 'DEU'| 'DZO'| 'ELL'| 'ENG'| 'ENM'| 'EPO'| 'EST'| 'EUS'| 'FAS'| 'FIN'| 'FRA'| 'FRK'| 'FRM'| 'GLE'| 'GLG'| 'GRC'| 'GUJ'| 'HAT'| 'HEB'| 'HIN'| 'HRV'| 'HUN'| 'IKU'| 'IND'| 'ISL'| 'ITA'| 'ITA_OLD'| 'JAV'| 'JPN'| 'KAN'| 'KAT'| 'KAT_OLD'| 'KAZ'| 'KHM'| 'KIR'| 'KOR'| 'KUR'| 'LAO'| 'LAT'| 'LAV'| 'LIT'| 'MAL'| 'MAR'| 'MKD'| 'MLT'| 'MSA'| 'MYA'| 'NEP'| 'NLD'| 'NOR'| 'ORI'| 'PAN'| 'POL'| 'POR'| 'PUS'| 'RON'| 'RUS'| 'SAN'| 'SIN'| 'SLK'| 'SLV'| 'SPA'| 'SPA_OLD'| 'SQI'| 'SRP'| 'SRP_LATN'| 'SWA'| 'SWE'| 'SYR'| 'TAM'| 'TEL'| 'TGK'| 'TGL'| 'THA'| 'TIR'| 'TUR'| 'UIG'| 'UKR'| 'URD'| 'UZB'| 'UZB_CYRL'| 'VIE'| 'YID';
+  const languages: Record<LanguageKey, string>;
 }
 
 export = Tesseract;
